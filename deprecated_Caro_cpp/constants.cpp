e86794b0c41182b6a645e4e7cc742b2be194daa2
Dien Nguyen <dienn1@uci.edu>
2022-11-11 18:51:59 -0800

    remove caro cython and old caro cpp folder

--- deprecated_Caro_cpp/constants.cpp
+++
@@ -1,9 +0,0 @@
-#include "constants.h"
-
-namespace constants {
-    extern const double C = sqrt(2);    // EXPLORATION PARAMETER
-
-    extern const int MAX_DIM {30};
-    extern const std::string CHAR[3] {"O", ".", "X"};
-    extern const std::string *CHAR_P {&CHAR[1]}; // this is to be able to use negative index e.g. CHAR_P[-1] = "O" and CHAR_P[1] = "X"
-}
