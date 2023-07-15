943c63426d75930da19e7ccb9f9853fb2c6d3d30
Dien Nguyen <dienn1@uci.edu>
2023-07-14 18:44:06 -0700

    Final Update

---
+++ Model_pybind.pyi
@@ -0,0 +1,10 @@
+class SmallNet:
+    def __init__(self, conv1Weights, conv1Bias, conv2Weights, conv2Bias,
+                 fc1Weights, fc1Bias, fc2Weights, fc2Bias, fc3Weights, fc3Bias):
+        pass
+
+    def forward(self, input):
+        pass
+
+    def __call__(self, input):
+        pass
